c507686104ca76950538bd502c622d3ee49c108e
Vaishali-962 <vaishaliwankhade69200@gmail.com>
2026-02-26 12:14:08 +0530

    Added Required fields and annotations to EmployeeResponseDto class

--- src/main/java/com/GenNext/employee_management/dto/EmployeeResponseDto.java
+++ src/main/java/com/GenNext/employee_management/dto/EmployeeResponseDto.java
@@ -1,6 +1,25 @@
 package com.GenNext.employee_management.dto;
 
 
-public class EmployeeResponseDto {
+import com.GenNext.employee_management.enums.Department;
+import com.GenNext.employee_management.enums.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Builder;
+import lombok.Data;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class EmployeeResponseDto {
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private Role designation;
+    @Enumerated(EnumType.STRING)
+    private Department department;
+    private BigDecimal salary;
+    private String mobileNumber;
+    private LocalDate joinOn;
 }
